bc08356edc7d74bf6049fbd67db355fd2191d5ef
Lucas Machado <74425619+Dev-Machado05@users.noreply.github.com>
2025-03-20 20:00:06 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,6 @@ export default function App() {
 
   const changeDisplayCart = () => {
     setCartDisplay(!CartDisplay);
-    document.body.style.overflow = CartDisplay ? "" : "hidden";
   };
 
   useEffect(() => {
